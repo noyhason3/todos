cfbbcaa249b185368f0e0861aa819f6c787f63ed
noy <noyhason3@gmail.com>
2022-08-05 13:20:12 +0300

    basic structure + rotuer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FunctionComponent } from 'react';
-import logo from './logo.svg';
+import { Home } from './pages/Home/Home';
+import { Todo } from './pages/Todo/Todo';
 import './App.scss';
 
 export const App: FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+
+export const Home: FunctionComponent = () => {
+
+    return <section className="home">
+        <h1>Home</h1>
+    </section>
+}
